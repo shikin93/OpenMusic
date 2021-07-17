b2014849439294a06185d9d98254767277286b59
shikin93 <official.shiin@gmail.com>
2021-07-17 14:47:44 +0700

    delete getUserById

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -2,7 +2,6 @@ const { Pool } = require('pg');
 const { nanoid } = require('nanoid');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
-const NotFoundError = require('../../exceptions/NotFoundError');
 
 class UsersService {
   constructor() {
@@ -33,18 +32,6 @@ class UsersService {
     if (result.rowCount > 0) {
       throw new InvariantError('Gagal menambahkan user. Username sudah digunakan');
     }
-  }
-
-  async getUserById(userId) {
-    const query = {
-      text: 'SELECT id, username, fullname, FROM users WHERE id = $1',
-      values: [userId],
-    };
-    const result = await this._pool.query(query);
-    if (!result.rows.length) {
-      throw new NotFoundError('User tidak ditemukan');
-    }
-    return result.rows[0];
   }
 }
 
