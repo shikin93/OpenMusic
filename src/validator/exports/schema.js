a43d35385cd6bdb82635e1db7db341e47f84c723
shikin93 <official.shiin@gmail.com>
2021-07-28 01:35:21 +0700

    add schema export validator

---
+++ src/validator/exports/schema.js
@@ -0,0 +1,11 @@
+const Joi = require('joi');
+
+const ExportSongsPayloadSchema = Joi.object({
+  targetEmail: Joi.string()
+    .email({
+      tlds: true,
+    })
+    .required(),
+});
+
+module.exports = ExportSongsPayloadSchema;
