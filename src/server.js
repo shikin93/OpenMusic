9866a965340c14c9ba222e74d52978de56effce2
shikin93 <official.shiin@gmail.com>
2021-07-29 06:46:37 +0700

    add Inert plugin

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
+const Inert = require('@hapi/inert');
 const path = require('path');
 
 const CLientError = require('./exceptions/ClientError');
@@ -87,6 +88,9 @@ const init = async () => {
   await server.register([
     {
       plugin: Jwt,
+    },
+    {
+      plugin: Inert,
     },
   ]);
 
