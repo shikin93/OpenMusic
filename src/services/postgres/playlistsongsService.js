b4c8e14c8304f7856ad9e0f8c66e9743192ab059
shikin93 <official.shiin@gmail.com>
2021-07-30 09:21:10 +0700

    implemented cache on addPlaylistsongs, getPlaylistsongs, deleteSongInPlaylist

--- src/services/postgres/playlistsongsService.js
+++ src/services/postgres/playlistsongsService.js
@@ -3,8 +3,9 @@ const { Pool } = require('pg');
 const InvariantError = require('../../exceptions/InvariantError');
 
 class PlaylistsongsService {
-  constructor() {
+  constructor(cacheService) {
     this._pool = new Pool();
+    this._cacheService = cacheService;
   }
 
   async addPlaylistsongs(playlistId, songId) {
@@ -18,16 +19,28 @@ class PlaylistsongsService {
     if (!result.rowCount) {
       throw new InvariantError('Lagu gagal ditambahkan ke playlists, lagu tidak ditemukan');
     }
+    await this._cacheService.delete(`playlistSongs:${playlistId}`);
     return result.rows[0].id;
   }
 
   async getPlaylistsongs(owner) {
-    const query = {
-      text: 'SELECT music.id, music.title, music.performer FROM music LEFT JOIN playlistsongs ON playlistsongs.song_id = music.id LEFT JOIN playlists ON playlists.id = playlistsongs.playlist_id LEFT JOIN collaborations ON collaborations.playlist_id = playlists.id WHERE playlists.owner = $1 OR collaborations.user_id = $1',
-      values: [owner],
-    };
-    const result = await this._pool.query(query);
-    return result.rows;
+    try {
+      // mendapatakan playlistsongs dari cache
+      const result = await this._cacheService.get(`playlist:${owner}`);
+      return JSON.parse(result);
+    } catch (err) {
+      // bila gagal ambil dari database
+      const query = {
+        text: 'SELECT music.id, music.title, music.performer FROM music LEFT JOIN playlistsongs ON playlistsongs.song_id = music.id LEFT JOIN playlists ON playlists.id = playlistsongs.playlist_id LEFT JOIN collaborations ON collaborations.playlist_id = playlists.id WHERE playlists.owner = $1 OR collaborations.user_id = $1',
+        values: [owner],
+      };
+      const result = await this._pool.query(query);
+      const mappedResult = result.rows;
+
+      // playlistsongs akan disimpan pada cache sebelum fungsi getPlaylistsongs dikembalikan
+      await this._cacheService.set(`playlistSongs:${owner}`, JSON.stringify(mappedResult));
+      return mappedResult;
+    }
   }
 
   async deleteSongInPlaylists(playlistId, songId) {
@@ -40,6 +53,7 @@ class PlaylistsongsService {
     if (!result.rowCount) {
       throw new InvariantError('Lagu gagal dihapus dari playlist, lagu tidak ditemukan');
     }
+    await this._cacheService.delete(`playlistSongs:${playlistId}`);
   }
 }
 
